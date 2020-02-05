74a2280add8fd553c1d68e26f74994b1286bc600
Engr. Muhammad Anis Ur rehman <33159177+Aneesawan34@users.noreply.github.com>
2020-02-05 23:04:20 +0500

    in this file javascript array that reverse specific string from elements

---
+++ index.js
@@ -0,0 +1,6 @@
+var myArry = ["abc, georgia", "abc, hello"];
+const items = myArry.forEach(val => {
+  let val2 = val.split("").splice(0, 3);
+  //   console.log(val2);
+  console.log(val2.reverse().join("") + val.slice(3));
+});
